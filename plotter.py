e00764859b85e877189a8173fa30f18a2072fcfa
sbuschmann <stephan.buschmann@gmail.com>
2018-12-18 21:28:20 +0100

    update plotter

--- plotter.py
+++ plotter.py
@@ -1,98 +1,140 @@
-#!/usr/bin/env python
-# encoding: utf-8
-
-import matplotlib
-matplotlib.use('Agg')
-
 import glob
 import os
 import pandas as pd
+import matplotlib
 import numpy as np
 import datetime as dt
 import time
+from matplotlib.dates import DateFormatter
+from matplotlib.dates import HourLocator
 import matplotlib.pyplot as plt
+from matplotlib.patches import Patch
+from matplotlib.lines import Line2D
+plt.style.use('default')
+%matplotlib inline  
 
 #from IPython.core.interactiveshell import InteractiveShell
 #InteractiveShell.ast_node_interactivity = "all"
 
 
-def remove_outliers(df, overwrite = False, q_upper = 0.99, q_lower = 0.01):
-    for col in df.columns:
-        col_derivative = "d%s" %col
-        df[col_derivative] = df[col].shift(-1)/df[col].shift(0)-1
-        lower = df[col_derivative].quantile(q_lower)
-        upper = df[col_derivative].quantile(q_upper)
-        f_lower = df[col_derivative]<lower
-        f_upper = df[col_derivative]>upper
-        f = f_lower | f_upper
-        df.loc[:, col]  = df.loc[:, col].copy(deep = True)
-        df.loc[f, col] = np.nan
-        df.loc[f, col].fillna(method = "pad", inplace = True)
-        df.drop(col_derivative, axis = 1, inplace = True)
-    return df
+def remove_outliers(df, overwrite = False, q_upper = 0.99, q_lower = 0.01):   
+    sensors = df.sensor.unique()
+    for sensor in sensors:
+        f_sensor = df["sensor"] == sensor 
+        for col in df.columns:
+            col_derivative = "d%s" %col
+            df.loc[f_sensor, col_derivative] = df.loc[f_sensor, col].shift(-1)/df.loc[f_sensor, col].shift(0)-1
+            lower = df.loc[f_sensor, col_derivative].quantile(q_lower)
+            upper = df.loc[f_sensor, col_derivative].quantile(q_upper)
+            f_lower = df.loc[f_sensor, col_derivative]<lower
+            f_upper = df.loc[f_sensor, col_derivative]>upper
+            f = f_lower | f_upper
+            df.loc[f_sensor, col]  = df.loc[f_sensor, col].copy(deep = True)
+            df.loc[f_sensor & f, col] = np.nan
+            df.loc[f_sensor & f, col].fillna(method = "pad", inplace = True)
+            df.drop(col_derivative, axis = 1, inplace = True)
+    #return df
 
-
-
-#path = r"/Users/stephan/prog/weather_station"
-path = r"."
-
-frames = []
-
-print("Generating weather plots....")
-while True:
-    start = dt.datetime.now()
-    time.sleep(60)
-    for file in glob.glob(os.path.join(path, "*.csv")):
-        frames.append(pd.read_csv(file, header = None, names = ["time", "Temp", "Hum"]))
-    df = pd.concat(frames)
+def load_data(path = r"/Volumes/pi/prog/weather_station"):
+    frames = []
+    files = glob.glob(os.path.join(path, "data", "weather_data_*.csv"))
+    for file in files[:]:
+        print(file)
+        frames.append(pd.read_csv(file))
+    data = pd.concat(frames)
+    df = data.copy(deep = True)
 
     df["time"] = pd.to_datetime(df["time"])
     df = df.set_index("time")
     df = df.sort_index()
+    df = df.loc[df.index.notnull()]
+    return df
 
-    df = remove_outliers(df)
+def plot_weather_data():
+    #remove_outliers(df)
+
+    fig, axs = plt.subplots(2,1
+                           # , figsize=(10, 10)
+                           )
+    #fig.suptitle("hhuhu")
+
 
     last = "1D"
     freq = "10T"
 
-    fig, axs = plt.subplots(2, 1)
+    lineWidth = 2
+
+    n=0
+
+    sensor_in = 4
+    sensor_out = 17
+
+    last_temp_in = df.loc[df.sensor==sensor_in, "temp"][-1]
+    last_temp_out = df.loc[df.sensor==sensor_out, "temp"][-1]
+    last_hum_in = df.loc[df.sensor==sensor_in, "hum"][-1]
+    last_hum_out = df.loc[df.sensor==sensor_out, "hum"][-1]
+    last_time = df.index[-1].strftime("%H:%M:%S")
 
 
-    temp = df["Temp"]
-    hum = df["Hum"]
+    legend_elements = [Line2D([0], [0], color='r', lw=0, label='Temp In %s' %last_temp_in),
+                       Line2D([0], [0], color='w', lw=0, label='Temp Out %s' %last_temp_out)]
 
-    tmp = temp.last(last)
-    tmp = tmp.last(last).groupby(pd.Grouper(freq=freq)).mean()
+    now = dt.datetime.now()
+    annotation = ""
+    #axs[0].annotate("%s %.2f° %.2f%%" %(dt.datetime.now().strftime("%d.%m.%Y %H:%M:%S"), temp_last_val, hum_last_val)
+    #                , xy = (0.25, 1.1)
+    #                , xycoords = "axes fraction"
+    #                , style="normal"
+    #                #, bbox=dict(boxstyle="square", fc="w", ec="k")
+    #               )
 
-    temp_last_val = temp[-1]
-    hum_last_val = hum[-1]
+    what_to_plot = "temp"
+    f = df["sensor"]==sensor_in
+    tmp = df.loc[f].last(last)
+    tmp = tmp.groupby(pd.Grouper(freq=freq)).mean()
+    axs[n].plot(tmp.index, tmp[what_to_plot], "-", lw=lineWidth)
 
-    axs[0].annotate("%s\n%.2f°\n%.2f%%" %(dt.datetime.now().strftime("%d.%m.%Y %H:%M:%S"), temp_last_val, hum_last_val)
-                    , xy = (0.5, 0.6)
-                    , xycoords = "axes fraction"
-                    , style="normal"
-                    , bbox=dict(boxstyle="square", fc="w", ec="k")
-                   )
+    f = df["sensor"]==sensor_out
+    tmp = df.loc[f].last(last)
+    tmp = tmp.groupby(pd.Grouper(freq=freq)).mean()
+    axs[n].plot(tmp.index, tmp[what_to_plot], "-", lw=lineWidth)
 
-    axs[0].plot(tmp.index, tmp.values, "-", lw=2, color="orange")
-    axs[0].grid(True)
-    axs[0].set_ylabel("Temperature")
-    axs[0].get_xaxis().set_ticklabels([])
+    axs[n].grid(True)
+    axs[n].set_ylabel("Temperature [°C]")
+    axs[n].get_xaxis().set_ticklabels([])
+    axs[n].set_ylim(-5,25)
+    #axs[n].text(last_time, 10, "huhu")
+    #axs[n].legend(handles=legend_elements, loc='right')
+    lgd1 = axs[n].legend(["In \nAktuell %s°" %last_temp_in, "Out\nAktuell %s°" %last_temp_out], 
+                  loc = "upper center", bbox_to_anchor=(1.17, 1))
 
-    tmp = hum.last(last)
-    tmp = tmp.last(last).groupby(pd.Grouper(freq=freq)).mean()
-    #hum_last_val = tmp[-1]
-    #axs[1].annotate("Aktuelle Luftfeuchtigkeit \n%.2f%%" %hum_last_val, xy = (1.1, 0.8), xycoords = "axes fraction")
-    axs[1].grid(True)
-    axs[1].set_ylim(45, 60)
-    axs[1].set_ylabel("Humidity")
-    axs[1].set_xlabel("Zeit")
-    axs[1].plot(tmp.index, tmp.values, "-", lw=2)
+    n=1
+    what_to_plot = "hum"
+    f = df["sensor"]==sensor_in
+    tmp = df.loc[f].last(last)
+    tmp = tmp.groupby(pd.Grouper(freq=freq)).mean()
+    axs[n].plot(tmp.index, tmp[what_to_plot], "-", lw=lineWidth)
 
-    #fig.tight_layout()
+    f = df["sensor"]==sensor_out
+    tmp = df.loc[f].last(last)
+    tmp = tmp.groupby(pd.Grouper(freq=freq)).mean()
+    axs[n].plot(tmp.index, tmp[what_to_plot], "-", lw=lineWidth)
+
+    axs[n].grid(True)
+    axs[n].set_ylabel("Humidity [%]")
+    axs[n].set_xlabel("Uhrzeit")
+    axs[n].xaxis.set_major_formatter(DateFormatter('%H:%M'))
+    #axs[n].yaxis.set_major_formatter(DateFormatter('%d%%'))
+    #axs[n].xaxis.set_major_locator(HourLocator(np.arange(0,25,1)))
+
+    lgd2 = axs[n].legend(["In \nAktuell %s%%" %last_hum_in, "Out\nAktuell %s%%" %last_hum_out],
+                  loc = "upper center", bbox_to_anchor=(1.17, 1))
+
     fig.autofmt_xdate()
+    fig.savefig(os.path.join(path, "fig.png"), bbox_extra_artists = (lgd1,lgd2), dpi=200, bbox_inches='tight')
 
-    fig.savefig(os.path.join(path, "weather_info.png"), dpi = 200)
+path = r"/Volumes/pi/prog/weather_station"
+path = r"."
 
-    end = dt.datetime.now()
-    print("runtime %s" %(end-start))
+df = load_data(path)
+plot_weather_data()
