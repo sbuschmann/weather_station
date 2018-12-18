c764828c1ebcf88ba97fd8e9b32a8939b9aaa324
sbuschmann <stephan.buschmann@gmail.com>
2018-12-18 21:15:53 +0100

    updated harvester

--- harvester.py
+++ harvester.py
@@ -1,43 +1,50 @@
+#stephan
+
 import datetime as dt
 import Adafruit_DHT as ada
-import time 
+import time
 import sys
+import numpy as np
 
 sensor = ada.DHT22
-pin = sys.argv[1]
+pins = [4,17]
 i=0
 
 now = dt.datetime.now().strftime(format="%Y%m%d_%H%M%S")
-f = open("./data/weather_data_%s_%s.csv" %(pin, now), "a+")
+f = open("./data/weather_data_%s.csv" %(now), "a+")
 header = "time,sensor,temp,hum\n"
 f.write(header)
 
-h_old = 0
-h_new = 0
-t_old = 0
-t_new = 0
+nPins = len(pins)
 
-print("WeatherStation is running on pin %s ..." %pin)
+h_old = np.zeros(nPins)
+h_new = np.zeros(nPins)
+t_old = np.zeros(nPins)
+t_new = np.zeros(nPins)
+
+print("WeatherStation is running ...")
 
 while True:
-	time.sleep(2)
+	#time.sleep(10)
 	i +=1
-	now = dt.datetime.now()	
-	h_new, t_new = ada.read_retry(sensor, pin)
+	now = dt.datetime.now()
 
-	h_new = "%.4f" %h_new
-	t_new = "%.4f" %t_new
+	for i in range(0,nPins):
+		pin = pins[i]
 
-	if (h_new != h_old) or (t_new != t_old):
-		line = "%s,%s,%s,%s\n" %(now, pin, t_new, h_new)
-		f.write(line)
-		f.flush()
-		print("Pin %s: T or H changed to %s and %s at %s" %(pin, t_new, h_new, now))	
-		h_old = h_new
-		t_old = t_new
-	else:
-		print("T and H on pin %s unchanged" %pin)	
+		h_new[i], t_new[i] = ada.read_retry(sensor, pin)
 
-	if i == -10:
-		break
+		h_new[i] = "%.4f" %h_new[i]
+		t_new[i] = "%.4f" %t_new[i]
+
+		if (h_new[i] != h_old[i]) or (t_new[i] != t_old[i]):
+			line = "%s,%s,%s,%s\n" %(now, pin, t_new[i], h_new[i])
+			f.write(line)
+			f.flush()
+			print("Pin %s: T or H changed to %s and %s at %s" %(pin, t_new[i], h_new[i], now))
+			h_old[i] = h_new[i]
+			t_old[i] = t_new[i]
+		else:
+			print("T and H on pin %s unchanged" %pin)
+	time.sleep(10)
 f.close()
